0796bf490049b8651da22161992dcaef8c460ff5
Eric(Yinze) <yinze.liu@mendix.com>
2026-08-04 15:13:32 +0200

    Hide resume button

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -39,14 +39,14 @@ export const Interface = (props) => {
       <SkillsSection />
       <ProjectsSection />
       <ContactSection />
-      <button
+      {/* <button
         onClick={() => {
           window.open("/files/Frontend_Engineer_Yinze.pdf", "_blank");
         }}
         className="bg-sky-800 fixed bottom-[60px] right-[60px] text-white py-4 px-8 rounded-[20px] font-bold text-lg mt-16  hover:bg-sky-700"
       >
         My Resume
-      </button>
+      </button> */}
     </div>
   );
 };
